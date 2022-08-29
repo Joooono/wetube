4a960caae748bbe9155f55b9faa9d6683a10e37c
Joooono <bajune1192@naver.com>
2022-08-30 02:15:07 +0900

    Summary

---
+++ src/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { isNamedExportBindings } from "typescript";
+
+const PORT = 4000;
+
+const app = express();
+
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>")
+    }
+    console.log("Allowed, you may continue");
+    next();
+}
+
+const handleHome = (req, res, next) => {
+    return res.send("I love middlewares");
+}
+
+const handleProtected = (req, res, next) => {
+    return res.send("Welcome to the private lounge.")
+}
+
+/*const handleLogin = (req,res) => {
+    return res.send("Login here.")
+}*/
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+/*app.get("/login", handleLogin);*/
+
+const handleListening = () => console.log(`Server Listening on port http://localhost${PORT}`);
+
+app.listen(PORT, handleListening);
+
